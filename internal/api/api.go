799656d288ed903e462c8b4b3ef18d5fb6565335
luiz bueno <luiz504@hotmail.com>
2024-08-06 09:24:56 -0300

    feat: create routes and integrate middlares

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/luiz504/week-tech-go-server/internal/store/pg"
 )
 
@@ -20,9 +21,45 @@ func NewHandler(q *pg.Queries) http.Handler {
 	a := apiHandler{
 		q: q,
 	}
+
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	r.Get("/subscribe/{room_id}", a.handleSubscribeToRoom)
+
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/rooms", func(r chi.Router) {
+			r.Post("/", a.handleCreateRoom)
+			r.Get("/", a.handleGetRooms)
+
+			r.Route("/{room_id}/messages", func(r chi.Router) {
+				r.Post("/", a.handleCreateRoomMessage)
+				r.Get("/", a.handleGetRoomMessages)
+
+				r.Route("/{message_id}", func(r chi.Router) {
+					r.Get("/", a.handleGetRoomMessage)
+					r.Patch("/react", a.handleReactToMessage)
+					r.Delete("/react", a.handleRemoveReactionFromMessage)
+					r.Patch("/answer", a.handleMarkMessageAsAnswered)
+				})
+
+			})
+		})
+	})
 
 	a.r = r
 
 	return a
 }
+
+func (h apiHandler) handleSubscribeToRoom(w http.ResponseWriter, r *http.Request) {}
+
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {}
+func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)   {}
+
+func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request)         {}
+func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request)           {}
+func (h apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request)            {}
+func (h apiHandler) handleReactToMessage(w http.ResponseWriter, r *http.Request)            {}
+func (h apiHandler) handleRemoveReactionFromMessage(w http.ResponseWriter, r *http.Request) {}
+func (h apiHandler) handleMarkMessageAsAnswered(w http.ResponseWriter, r *http.Request)     {}
